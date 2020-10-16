5b1f3b1e9eef07dc84b56d72d1a74117b782612d
Michael Watts <wattsjmichael@gmail.com>
2020-10-16 08:44:41 -0700

    Add FactoryContext Model

---
+++ Factory/Models/FactoryContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace Factory.Models
+{
+  public class FactoryContext : DbContext
+  {
+    public virtual DbSet<Engineer> Engineers {get; set;}
+    public virtual DbSet<Machine> Machines {get; set;}
+
+    public DbSet<EngineerMachine> EngineerMachine {get; set;}
+
+    public FactoryContext(DbContextOptions options) : base (options) { }
+  }
+}
