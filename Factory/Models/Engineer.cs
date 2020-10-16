da1e664f4d138be1e994d81802b3c299234ecb1f
Michael Watts <wattsjmichael@gmail.com>
2020-10-16 08:34:39 -0700

    add EngineerModel

---
+++ Factory/Models/Engineer.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+
+namespace Factory.Models
+{
+  public class Engineer
+  {
+    public Engineer()
+    {
+      thisMachines = new HashSet<EngineerMachine>();
+    }
+    public int EngineerId { get; set;}
+    public string EngineerName {get; set;}
+
+    public string EngineerDate {get; set;}
+
+    
+    public ICollection<EngineerMachine> Machines {get;}
+
+  }
+}
