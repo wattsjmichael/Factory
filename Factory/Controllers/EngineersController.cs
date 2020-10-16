25b3f6d2cdb806b714c5fbb82c2d6562406d4220
Michael Watts <wattsjmichael@gmail.com>
2020-10-16 09:15:33 -0700

    add HomeController.cs

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Hospital.Models;
+
+namespace
